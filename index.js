9c9dd726b71e093451bc68b73b3ce28cea3ac1b0
moisesantunes <tec.m.antunes@gmail.com>
2019-06-26 14:04:09 -0300

    primeiro estagio da criacao das rotas get

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var path = require('path');
 
 const servo =http.createServer(function (req, res) {
  // res.end();
 })
 servo.on('request',(req,res)=>{
-const ps = fs.readdirSync("./paginas");
-let pagina = "./paginas"+req.url;
-console.log(pagina)
-console.log(ps)
+//const ps = fs.readdirSync("./paginas");
+//let pagina = "./paginas"+req.url;
+//console.log(pagina)
+//console.log(ps)
 
-	if(req.url == "/"){
-		pagina = pagina+"index.html";
-		
-	}
+	let pagina = './paginas' + req.url;
+	
+	    if (pagina == './paginas/') {
+	        pagina = './paginas/index.html';
+	    }else if(req.url == "/inverno"){
+	    	pagina = "./paginas/inverno.html"
+	    }
+	console.log(pagina)
+	    var extensao = String(path.extname(pagina)).toLowerCase();
+	    var mimeTypes = {
+	        '.html': 'text/html',
+	        '.js': 'text/javascript',
+	        '.css': 'text/css',
+	        '.json': 'application/json',
+	        '.png': 'image/png',
+	        '.jpg': 'image/jpg',
+	        '.gif': 'image/gif',
+	        '.wav': 'audio/wav',
+	        '.mp4': 'video/mp4',
+	        '.woff': 'application/font-woff',
+	        '.ttf': 'application/font-ttf',
+	        '.eot': 'application/vnd.ms-fontobject',
+	        '.otf': 'application/font-otf',
+	        '.svg': 'application/image/svg+xml',
+	        '.wasm': 'application/wasm'
+	    };
+	
+	    var contentType = mimeTypes[extensao] || 'application/octet-stream';
+	    fs.readFile(pagina, function(error, content) {
+	        if (error) {
+	            if(error.code == 'ENOENT') {
+	                fs.readFile('./404.html', function(error, content) {
+	                    res.writeHead(404, { 'Content-Type': contentType });
+	                    res.end(content, 'utf-8');
+	                });
+	            }
+	            else {
+	                res.writeHead(500);
+	                res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+	                res.end();
+	            }
+	        }
+	        else {
+	            res.writeHead(200, { 'Content-Type': contentType });
+	            res.end(content, 'utf-8');
+	        }
+	    });
 
 	
 /*
